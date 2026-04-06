fbb9fb3ad5d9b04f79589123c73b8e68e00a978d
yaichnybelok <roman.makzhanov@mail.ru>
2026-04-06 23:41:57 +0300

    4x4 WIP

--- app/src/main/java/com/example/project/Game.kt
+++ app/src/main/java/com/example/project/Game.kt
@@ -484,6 +484,288 @@ class Game : AppCompatActivity() {
             val newItem: View = inflater.inflate(R.layout.activity_4x4, gameLayout, false)
             gameLayout.addView(newItem)
 
+            val text_1_2__10 = newItem.findViewById<TextView>(R.id.text_1_2__10)
+            val text_1_3__10 = newItem.findViewById<TextView>(R.id.text_1_3__10)
+            val text_1_4__10 = newItem.findViewById<TextView>(R.id.text_1_4__10)
+            val text_1_5__10 = newItem.findViewById<TextView>(R.id.text_1_5__10)
+            val text_1_8__10 = newItem.findViewById<TextView>(R.id.text_1_8__10)
+            val text_2_1__10 = newItem.findViewById<TextView>(R.id.text_2_1__10)
+            val text_2_7__10_right = newItem.findViewById<TextView>(R.id.text_2_7__10_right)
+            val text_2_7__10_down = newItem.findViewById<TextView>(R.id.text_2_7__10_down)
+            val text_3_1__10 = newItem.findViewById<TextView>(R.id.text_3_1__10)
+            val text_3_6__10_right = newItem.findViewById<TextView>(R.id.text_3_6__10_right)
+            val text_3_6__10_down = newItem.findViewById<TextView>(R.id.text_3_6__10_down)
+            val text_3_3__10_right = newItem.findViewById<TextView>(R.id.text_3_3__10_right)
+            val text_3_3__10_down = newItem.findViewById<TextView>(R.id.text_3_3__10_down)
+            val text_3_10__10 = newItem.findViewById<TextView>(R.id.text_3_10__10)
+            val text_4_1__10 = newItem.findViewById<TextView>(R.id.text_4_1__10)
+            val text_4_4__10_right = newItem.findViewById<TextView>(R.id.text_4_4__10_right)
+            val text_4_4__10_down = newItem.findViewById<TextView>(R.id.text_4_4__10_down)
+            val text_4_8__10_right = newItem.findViewById<TextView>(R.id.text_4_8__10_right)
+            val text_4_8__10_down = newItem.findViewById<TextView>(R.id.text_4_8__10_down)
+            val text_5_2__10 = newItem.findViewById<TextView>(R.id.text_5_2__10)
+            val text_5_5__10_right = newItem.findViewById<TextView>(R.id.text_5_5__10_right)
+            val text_5_5__10_down = newItem.findViewById<TextView>(R.id.text_5_5__10_down)
+            val text_5_7__10 = newItem.findViewById<TextView>(R.id.text_5_7__10)
+            val text_6_3__10_right = newItem.findViewById<TextView>(R.id.text_6_3__10_right)
+            val text_6_3__10_down = newItem.findViewById<TextView>(R.id.text_6_3__10_down)
+            val text_6_7__10 = newItem.findViewById<TextView>(R.id.text_6_7__10)
+            val text_6_9__10 = newItem.findViewById<TextView>(R.id.text_6_9__10)
+            val text_6_10__10 = newItem.findViewById<TextView>(R.id.text_6_10__10)
+            val text_7_2__10 = newItem.findViewById<TextView>(R.id.text_7_2__10)
+            val text_7_7__10 = newItem.findViewById<TextView>(R.id.text_7_7__10)
+            val text_8_2__10_right = newItem.findViewById<TextView>(R.id.text_8_2__10_right)
+            val text_8_2__10_down = newItem.findViewById<TextView>(R.id.text_8_2__10_down)
+            val text_8_8__10_right = newItem.findViewById<TextView>(R.id.text_8_8__10_right)
+            val text_8_8__10_down = newItem.findViewById<TextView>(R.id.text_8_8__10_down)
+            val text_8_5__10 = newItem.findViewById<TextView>(R.id.text_8_5__10)
+            val text_8_7__10 = newItem.findViewById<TextView>(R.id.text_8_7__10)
+            val text_9_1__10 = newItem.findViewById<TextView>(R.id.text_9_1__10)
+            val text_9_4__10_right = newItem.findViewById<TextView>(R.id.text_9_4__10_right)
+            val text_9_4__10_down = newItem.findViewById<TextView>(R.id.text_9_4__10_down)
+            val text_9_6__10 = newItem.findViewById<TextView>(R.id.text_9_6__10)
+            val text_10_1__10 = newItem.findViewById<TextView>(R.id.text_10_1__10)
+            val text_10_6__10 = newItem.findViewById<TextView>(R.id.text_10_6__10)
+
+
+            val text_1_2__2 = newItem.findViewById<TextView>(R.id.text_1_2__2)
+            val text_1_3__2 = newItem.findViewById<TextView>(R.id.text_1_3__2)
+            val text_1_4__2 = newItem.findViewById<TextView>(R.id.text_1_4__2)
+            val text_1_5__2 = newItem.findViewById<TextView>(R.id.text_1_5__2)
+            val text_1_8__2 = newItem.findViewById<TextView>(R.id.text_1_8__2)
+            val text_2_1__2 = newItem.findViewById<TextView>(R.id.text_2_1__2)
+            val text_2_7__2_right = newItem.findViewById<TextView>(R.id.text_2_7__2_right)
+            val text_2_7__2_down = newItem.findViewById<TextView>(R.id.text_2_7__2_down)
+            val text_3_1__2 = newItem.findViewById<TextView>(R.id.text_3_1__2)
+            val text_3_6__2_right = newItem.findViewById<TextView>(R.id.text_3_6__2_right)
+            val text_3_6__2_down = newItem.findViewById<TextView>(R.id.text_3_6__2_down)
+            val text_3_3__2_right = newItem.findViewById<TextView>(R.id.text_3_3__2_right)
+            val text_3_3__2_down = newItem.findViewById<TextView>(R.id.text_3_3__2_down)
+            val text_3_10__2 = newItem.findViewById<TextView>(R.id.text_3_10__2)
+            val text_4_1__2 = newItem.findViewById<TextView>(R.id.text_4_1__2)
+            val text_4_4__2_right = newItem.findViewById<TextView>(R.id.text_4_4__2_right)
+            val text_4_4__2_down = newItem.findViewById<TextView>(R.id.text_4_4__2_down)
+            val text_4_8__2_right = newItem.findViewById<TextView>(R.id.text_4_8__2_right)
+            val text_4_8__2_down = newItem.findViewById<TextView>(R.id.text_4_8__2_down)
+            val text_5_2__2 = newItem.findViewById<TextView>(R.id.text_5_2__2)
+            val text_5_5__2_right = newItem.findViewById<TextView>(R.id.text_5_5__2_right)
+            val text_5_5__2_down = newItem.findViewById<TextView>(R.id.text_5_5__2_down)
+            val text_5_7__2 = newItem.findViewById<TextView>(R.id.text_5_7__2)
+            val text_6_3__2_right = newItem.findViewById<TextView>(R.id.text_6_3__2_right)
+            val text_6_3__2_down = newItem.findViewById<TextView>(R.id.text_6_3__2_down)
+            val text_6_7__2 = newItem.findViewById<TextView>(R.id.text_6_7__2)
+            val text_6_9__2 = newItem.findViewById<TextView>(R.id.text_6_9__2)
+            val text_6_10__2 = newItem.findViewById<TextView>(R.id.text_6_10__2)
+            val text_7_2__2 = newItem.findViewById<TextView>(R.id.text_7_2__2)
+            val text_7_7__2 = newItem.findViewById<TextView>(R.id.text_7_7__2)
+            val text_8_2__2_right = newItem.findViewById<TextView>(R.id.text_8_2__2_right)
+            val text_8_2__2_down = newItem.findViewById<TextView>(R.id.text_8_2__2_down)
+            val text_8_8__2_right = newItem.findViewById<TextView>(R.id.text_8_8__2_right)
+            val text_8_8__2_down = newItem.findViewById<TextView>(R.id.text_8_8__2_down)
+            val text_8_5__2 = newItem.findViewById<TextView>(R.id.text_8_5__2)
+            val text_8_7__2 = newItem.findViewById<TextView>(R.id.text_8_7__2)
+            val text_9_1__2 = newItem.findViewById<TextView>(R.id.text_9_1__2)
+            val text_9_4__2_right = newItem.findViewById<TextView>(R.id.text_9_4__2_right)
+            val text_9_4__2_down = newItem.findViewById<TextView>(R.id.text_9_4__2_down)
+            val text_9_6__2 = newItem.findViewById<TextView>(R.id.text_9_6__2)
+            val text_10_1__2 = newItem.findViewById<TextView>(R.id.text_10_1__2)
+            val text_10_6__2 = newItem.findViewById<TextView>(R.id.text_10_6__2)
+
+
+            val text_2_2_edit = findViewById<EditText>(R.id.text_2_2_edit)
+            val text_2_3_edit = findViewById<EditText>(R.id.text_2_3_edit)
+            val text_2_4_edit = findViewById<EditText>(R.id.text_2_4_edit)
+            val text_2_5_edit = findViewById<EditText>(R.id.text_2_5_edit)
+            val text_2_8_edit = findViewById<EditText>(R.id.text_2_8_edit)
+            val text_2_9_edit = findViewById<EditText>(R.id.text_2_9_edit)
+            val text_3_2_edit = findViewById<EditText>(R.id.text_3_2_edit)
+            val text_3_4_edit = findViewById<EditText>(R.id.text_3_4_edit)
+            val text_3_5_edit = findViewById<EditText>(R.id.text_3_5_edit)
+            val text_3_7_edit = findViewById<EditText>(R.id.text_3_7_edit)
+            val text_3_8_edit = findViewById<EditText>(R.id.text_3_8_edit)
+            val text_3_9_edit = findViewById<EditText>(R.id.text_3_9_edit)
+            val text_4_2_edit = findViewById<EditText>(R.id.text_4_2_edit)
+            val text_4_3_edit = findViewById<EditText>(R.id.text_4_3_edit)
+            val text_4_5_edit = findViewById<EditText>(R.id.text_4_5_edit)
+            val text_4_6_edit = findViewById<EditText>(R.id.text_4_6_edit)
+            val text_4_7_edit = findViewById<EditText>(R.id.text_4_7_edit)
+            val text_4_9_edit = findViewById<EditText>(R.id.text_4_9_edit)
+            val text_4_10_edit = findViewById<EditText>(R.id.text_4_10_edit)
+            val text_5_3_edit = findViewById<EditText>(R.id.text_5_3_edit)
+            val text_5_4_edit = findViewById<EditText>(R.id.text_5_4_edit)
+            val text_5_6_edit = findViewById<EditText>(R.id.text_5_6_edit)
+            val text_5_8_edit = findViewById<EditText>(R.id.text_5_8_edit)
+            val text_5_9_edit = findViewById<EditText>(R.id.text_5_9_edit)
+            val text_5_10_edit = findViewById<EditText>(R.id.text_5_10_edit)
+            val text_6_4_edit = findViewById<EditText>(R.id.text_6_4_edit)
+            val text_6_5_edit = findViewById<EditText>(R.id.text_6_5_edit)
+            val text_6_6_edit = findViewById<EditText>(R.id.text_6_6_edit)
+            val text_6_8_edit = findViewById<EditText>(R.id.text_6_8_edit)
+            val text_7_3_edit = findViewById<EditText>(R.id.text_7_3_edit)
+            val text_7_4_edit = findViewById<EditText>(R.id.text_7_4_edit)
+            val text_7_5_edit = findViewById<EditText>(R.id.text_7_5_edit)
+            val text_7_8_edit = findViewById<EditText>(R.id.text_7_8_edit)
+            val text_7_9_edit = findViewById<EditText>(R.id.text_7_9_edit)
+            val text_7_10_edit = findViewById<EditText>(R.id.text_7_10_edit)
+            val text_8_3_edit = findViewById<EditText>(R.id.text_8_3_edit)
+            val text_8_4_edit = findViewById<EditText>(R.id.text_8_4_edit)
+            val text_8_9_edit = findViewById<EditText>(R.id.text_8_9_edit)
+            val text_8_10_edit = findViewById<EditText>(R.id.text_8_10_edit)
+            val text_9_2_edit = findViewById<EditText>(R.id.text_9_2_edit)
+            val text_9_3_edit = findViewById<EditText>(R.id.text_9_3_edit)
+            val text_9_5_edit = findViewById<EditText>(R.id.text_9_5_edit)
+            val text_9_7_edit = findViewById<EditText>(R.id.text_9_7_edit)
+            val text_9_8_edit = findViewById<EditText>(R.id.text_9_8_edit)
+            val text_9_9_edit = findViewById<EditText>(R.id.text_9_9_edit)
+            val text_10_2_edit = findViewById<EditText>(R.id.text_10_2_edit)
+            val text_10_3_edit = findViewById<EditText>(R.id.text_10_3_edit)
+            val text_10_4_edit = findViewById<EditText>(R.id.text_10_4_edit)
+            val text_10_5_edit = findViewById<EditText>(R.id.text_10_5_edit)
+            val text_10_7_edit = findViewById<EditText>(R.id.text_10_7_edit)
+            val text_10_8_edit = findViewById<EditText>(R.id.text_10_8_edit)
+
+
+
+            if (bit == 0) {
+                val solutions = mutableListOf(1, 2, 4, 8)
+                solutions.shuffle()
+
+                val row1sum = solutions[0] + solutions[1] + solutions[2]
+                val row2sum = solutions[2]
+                val row3sum = solutions[2] + solutions[3]
+                val row4sum = solutions[0] + solutions[1] + solutions[3]
+                val row5sum = solutions[0] + solutions[1] + solutions[2]
+                val row6sum = solutions[0] + solutions[3]
+                val row7sum = solutions[0] + solutions[2]
+                val row8sum = solutions[0] + solutions[1] + solutions[2] + solutions[3]
+                val row9sum = solutions[1] + solutions[2]
+                val row10sum = solutions[1] + solutions[3]
+                val row11sum = solutions[0] + solutions[1] + solutions[2] + solutions[3]
+                val row12sum = solutions[0] + solutions[3]
+                val row13sum = solutions[1] + solutions[2] + solutions[3]
+                val row14sum = solutions[0] + solutions[1] + solutions[2] + solutions[3]
+                val row15sum = solutions[0] + solutions[1] + solutions[3]
+                val row16sum = solutions[1] + solutions[2]
+                val row17sum = solutions[0] + solutions[1]
+                val row18sum = solutions[2] + solutions[3]
+                val row19sum = solutions[2] + solutions[3]
+                val row20sum = solutions[0] + solutions[1]
+                val row21sum = solutions[1]
+
+
+                val col1sum = solutions[0] + solutions[1] + solutions[2] + solutions[3]
+                val col2sum = solutions[1]
+                val col3sum = solutions[2] + solutions[3]
+                val col4sum = solutions[0] + solutions[1]
+                val col5sum = solutions[0] + solutions[1] + solutions[2]
+                val col6sum = solutions[0] + solutions[1] + solutions[3]
+                val col7sum = solutions[1] + solutions[3]
+                val col8sum = solutions[1] + solutions[2]
+                val col9sum = solutions[0] + solutions[1] + solutions[2] + solutions[3]
+                val col10sum = solutions[0] + solutions[2]
+                val col11sum = solutions[0] + solutions[2] + solutions[3]
+                val col12sum = solutions[0]
+                val col13sum = solutions[3]
+                val col14sum = solutions[0] + solutions[1] + solutions[3]
+                val col15sum = solutions[2] + solutions[3]
+                val col16sum = solutions[0] + solutions[2] + solutions[3]
+                val col17sum = solutions[1]
+                val col18sum = solutions[0] + solutions[1] + solutions[2]
+                val col19sum = solutions[0] + solutions[1] + solutions[3]
+                val col20sum = solutions[1] + solutions[2]
+                val col21sum = solutions[1] + solutions[2]
+                val col22sum = solutions[2] + solutions[3]
+
+
+
+                fun setValues(summ: Int, int10: TextView, int2: TextView) {
+                    int10.text = summ.toString().padStart(3, ' ')
+                    int2.text = summ.toString(2).padStart(4, '0')
+                }
+
+                setValues(row1sum, text_1_2__10, text_1_2__2)
+                setValues(row2sum, text_1_3__10, text_1_3__2)
+                setValues(row3sum, text_1_4__10, text_1_4__2)
+                setValues(row4sum, text_1_5__10, text_1_5__2)
+                setValues(row5sum, text_3_6__10_right, text_3_6__2_right)
+                setValues(row6sum, text_2_7__10_right, text_2_7__2_right)
+                setValues(row7sum, text_1_8__10, text_1_8__2)
+                setValues(row7sum, text_1_8__10, text_1_8__2)
+                setValues(row9sum, text_3_10__10, text_3_10__2)
+                setValues(row10sum, text_3_3__10_right, text_3_3__2_right)
+                setValues(row11sum, text_4_4__10_right, text_4_4__2_right)
+                setValues(row12sum, text_5_5__10_right, text_5_5__2_right)
+                setValues(row13sum, text_4_8__10_right, text_4_8__2_right)
+                setValues(row14sum, text_6_3__10_right, text_6_3__2_right)
+                setValues(row15sum, text_6_9__10, text_6_9__2)
+                setValues(row16sum, text_6_10__10, text_6_10__2)
+                setValues(row17sum, text_8_2__10_right, text_8_2__2_right)
+                setValues(row18sum, text_8_5__10, text_8_5__2)
+                setValues(row19sum, text_8_7__10, text_8_7__2)
+                setValues(row20sum, text_8_8__10_right, text_8_8__2_right)
+                setValues(row21sum, text_9_4__10_right, text_9_4__2_right)
+
+
+                setValues(col1sum, text_2_1__10, text_2_1__2)
+                setValues(col2sum, text_3_1__10, text_3_1__2)
+                setValues(col3sum, text_4_1__10, text_4_1__2)
+                setValues(col4sum, text_5_2__10, text_5_2__2)
+                setValues(col5sum, text_6_3__10_down, text_6_3__2_down)
+                setValues(col6sum, text_7_2__10, text_7_2__2)
+                setValues(col7sum, text_8_2__10_down, text_8_2__2_down)
+                setValues(col8sum, text_9_1__10, text_9_1__2)
+                setValues(col9sum, text_10_1__10, text_10_1__2)
+                setValues(col10sum, text_3_3__10_down, text_3_3__2_down)
+                setValues(col11sum, text_4_4__10_down, text_4_4__2_down)
+                setValues(col12sum, text_5_5__10_down, text_5_5__2_down)
+                setValues(col13sum, text_9_4__10_down, text_9_4__2_down)
+                setValues(col14sum, text_3_6__10_down, text_3_6__2_down)
+                setValues(col15sum, text_2_7__10_down, text_2_7__2_down)
+                setValues(col16sum, text_5_7__10, text_5_7__2)
+                setValues(col17sum, text_6_7__10, text_6_7__2)
+                setValues(col18sum, text_7_7__10, text_7_7__2)
+                setValues(col19sum, text_9_6__10, text_9_6__2)
+                setValues(col20sum, text_10_6__10, text_10_6__2)
+                setValues(col21sum, text_4_8__10_down, text_4_8__2_down)
+                setValues(col22sum, text_8_8__10_down, text_8_8__2_down)
+
+
+                /*checkBtn.setOnClickListener {
+                    val txt2_3 = text_2_3_edit.text.toString().toIntOrNull() ?: 0
+
+
+                    if ((txt2_3 + txt2_4 + txt2_5 == col1sum.toString()
+                            .toIntOrNull()) && (txt3_3 + txt3_4 + txt3_5 == col2sum.toString()
+                            .toIntOrNull())
+                    ) {
+
+                        val starsXml = layoutInflater.inflate(R.layout.three_star, null)
+                        MaterialAlertDialogBuilder(
+                            this,
+                            com.google.android.material.R.style.ThemeOverlay_Material3_MaterialAlertDialog_Centered
+                        )
+                            .setTitle("Решение верно!")
+                            .setNeutralButton("На главную") { dialog, which ->
+                                val intent = Intent(this, MainActivity::class.java)
+                                startActivity(intent)
+                            }
+                            .setPositiveButton("Следующее задание") { dialog, which ->
+                                val intent = Intent(this, Game::class.java)
+                                startActivity(intent)
+                            }.setView(starsXml)
+                            .show()
+
+                    } else {
+
+                        corrTxt.text = "Решение неверно!\nОжидание нового решения..."
+
+                    }
+                }*/
+
+            }
+
+
+
         }
 
     }
